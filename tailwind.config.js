88191bb15cd7eaacf580209e7b906b2364be5a9b
Sheila-a <sheila.codes.m@gmail.com>
2025-02-22 12:14:46 +0100

    feat: CSS assignments

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// export const content = ['./*.html', './src/**/*.{js,jsx,ts,tsx,css}'];
+// export const theme = {
+//   extend: {},
+// };
+// export const plugins = [];
+
+module.exports = {
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx,css}'], // Make sure CSS files are included
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
